00e7f546ed8ea4c22fd16c1c981e192c9aea7863
Garus404 <texnuk17@gmail.com>
2025-06-18 16:43:03 +0500

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
-  base: "/valdemar-cyber-portfolio-site/",
+  base: "/PortfolioSite/",
   server: {
     host: "::",
     port: 8080,
